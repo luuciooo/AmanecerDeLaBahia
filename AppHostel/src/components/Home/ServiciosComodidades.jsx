e46ffce142258d4f365add1b8ee00d6e91ebb6b7
luuciooo <lucioabalos97@gmail.com>
2024-10-26 23:06:49 -0300

    pasitos

--- AppHostel/src/components/Home/ServiciosComodidades.jsx
+++ AppHostel/src/components/Home/ServiciosComodidades.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import { Box, List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material';
 import LocalCafeIcon from '@mui/icons-material/LocalCafe';
 import KitchenIcon from '@mui/icons-material/Kitchen';
 import TvIcon from '@mui/icons-material/Tv';
@@ -10,41 +10,37 @@ import AccessTimeIcon from '@mui/icons-material/AccessTime';
 const ServiciosComodidades = () => {
     return (
         <Box sx={{ padding: 3 }}>
-            <Typography variant="h4" gutterBottom>
-                Servicios y Comodidades
-            </Typography>
             <List>
-                <ListItem>
-                    <ListItemIcon><LocalCafeIcon /></ListItemIcon>
-                    <ListItemText primary="Desayuno con pan rico incluido" />
+                <ListItem sx={{  padding : '5vh 5vw'}}>
+                <ListItemText primary={<Typography variant="h5" sx={{textAlign: 'end', padding : '0 5vw 0 0'}}>Desayuno con pan rico incluido</Typography>} />
+                    <ListItemIcon ><LocalCafeIcon sx={{ fontSize: 60 }}/></ListItemIcon>
+                </ListItem>
+                <ListItem sx={{  padding : '5vh 5vw'}}>
+                    <ListItemIcon ><KitchenIcon sx={{ fontSize: 60 , padding : '0 5vw 0 0' }} /></ListItemIcon>
+                    <ListItemText primary={<Typography variant="h5">Cocina equipada para preparar tus propias comidas</Typography>} />
+                </ListItem>
+                <ListItem sx={{ justifyContent: 'flex-end' }}>
+                    <ListItemText primary={<Typography variant="body1">Salón común con TV y chimenea</Typography>} />
+                    <ListItemIcon sx={{ fontSize: 40 }}><TvIcon /></ListItemIcon>
                 </ListItem>
                 <ListItem>
-                    <ListItemIcon><KitchenIcon /></ListItemIcon>
-                    <ListItemText primary="Cocina equipada para preparar tus propias comidas" />
+                    <ListItemIcon sx={{ fontSize: 40 }}><FireplaceIcon /></ListItemIcon>
+                    <ListItemText primary={<Typography variant="body1">Chimenea en el salón</Typography>} />
+                </ListItem>
+                <ListItem sx={{ justifyContent: 'flex-end' }}>
+                    <ListItemText primary={<Typography variant="body1">Biblioteca y juegos de mesa</Typography>} />
+                    <ListItemIcon sx={{ fontSize: 40 }}><BookIcon /></ListItemIcon>
                 </ListItem>
                 <ListItem>
-                    <ListItemIcon><TvIcon /></ListItemIcon>
-                    <ListItemText primary="Salón común con TV y chimenea" />
+                    <ListItemIcon sx={{ fontSize: 40 }}><LocalLaundryServiceIcon /></ListItemIcon>
+                    <ListItemText primary={<Typography variant="body1">Servicio de lavandería y planchado</Typography>} />
                 </ListItem>
-                <ListItem>
-                    <ListItemIcon><FireplaceIcon /></ListItemIcon>
-                    <ListItemText primary="Chimenea en el salón" />
-                </ListItem>
-                <ListItem>
-                    <ListItemIcon><BookIcon /></ListItemIcon>
-                    <ListItemText primary="Biblioteca y juegos de mesa" />
-                </ListItem>
-                <ListItem>
-                    <ListItemIcon><LocalLaundryServiceIcon /></ListItemIcon>
-                    <ListItemText primary="Servicio de lavandería y planchado" />
-                </ListItem>
-                <ListItem>
-                    <ListItemIcon><AccessTimeIcon /></ListItemIcon>
-                    <ListItemText primary="Recepción 24 horas" />
+                <ListItem sx={{ justifyContent: 'flex-end' }}>
+                    <ListItemText primary={<Typography variant="body1">Recepción 24 horas</Typography>} />
+                    <ListItemIcon sx={{ fontSize: 40 }}><AccessTimeIcon /></ListItemIcon>
                 </ListItem>
             </List>
         </Box>
     );
 };
-
 export default ServiciosComodidades;
